15804a6d7fee554932581750c4ccfff3be4fb0b7
Raviteja Gandam <gandamraviteja@gmail.com>
2024-04-29 16:59:00 +0530

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Homepage from './pages/Home/Homepage';
 
 function App() {
   return (
     <div className="App">
-
+      <Homepage />
     </div>
   );
 }
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Homepage.css";
+import Navbar from "./../../components/Navbar/Navbar";
 const Homepage = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Homepage;
