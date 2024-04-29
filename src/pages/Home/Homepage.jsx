c3237832d619accc0af3357a5267c6a27cc29b95
Raviteja Gandam <gandamraviteja@gmail.com>
2024-04-29 16:15:23 +0530

    Folder Structure

---
+++ src/pages/Home/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Homepage.css";
+const Homepage = () => {
+  return <div className="home"></div>;
+};
+
+export default Homepage;
---
+++ src/pages/Login/Loginpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Login.css";
+const Loginpage = () => {
+  return <div className="login"></div>;
+};
+
+export default Loginpage;
